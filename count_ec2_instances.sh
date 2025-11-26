c05a087efc2a0a2e05a4b703646fdd28a997a3c1
Arumbunitha <arumbunitha@gmail.com>
2025-11-26 09:15:40 +0530

    triggered by n8n

--- count_ec2_instances.sh
+++ count_ec2_instances.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-AWS_REGION="ap-south-1"
-
-INSTANCE_COUNT=$(aws ec2 describe-instances \
-    --region "$AWS_REGION" \
-    --filters Name=instance-state-name,Values=running \
-    --query "Reservations[*].Instances[*].InstanceId" \
-    --output json | jq length)
-
-echo "Triggered by n8n $AWS_REGION = $INSTANCE_COUNT"
+echo "Triggered by n8n"
 
